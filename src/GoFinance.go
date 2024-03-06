2134793fa7c9a29db896dc4ec0e049b6c7b2c9b5
August Brandt <aubr@itu.dk>
2024-03-06 16:56:32 +0100

    feat: Handle when file flag is not set

--- src/GoFinance.go
+++ src/GoFinance.go
@@ -7,18 +7,23 @@ import (
 )
 
 func main() {
-	file := flag.String("file", "", "Path to the csv file with transaction history")
+	file := flag.String("file", "unset", "Path to the csv file with transaction history")
 	flag.Parse()
 	
+	if *file == "unset" {
+		panic("The file was not set")
+	}
+
 	fmt.Println("File is: " + *file)
 	
-	// test reader
-	if *file != "" {
-		transactionHistory := Reader.ReadTransactions(*file)
-		for i, transaction := range transactionHistory.Transactions {
-			fmt.Printf("Transaction %d\n\tAmount: %f\n\tType: %s\n\tSubtype: %s\n\tDescription: %s\n", 
-				i, transaction.Amount, transaction.Type, transaction.Subtype, transaction.Description)
-		}
-		
-	}
+	transactionHistory := Reader.ReadTransactions(*file)
+	showTransactions(transactionHistory)
 }
+
+func showTransactions(th Reader.TransactionHistory) {
+	for i, transaction := range th.Transactions {
+		fmt.Printf("Transaction %d\n\tAmount: %f\n\tType: %s\n\tSubtype: %s\n\tDescription: %s\n", 
+			i, transaction.Amount, transaction.Type, transaction.Subtype, transaction.Description)
+	} 
+}
+
