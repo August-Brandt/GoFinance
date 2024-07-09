0c1778b9b7c7fa9e7d5d2ed685edb36b633c5f31
August-Brandt <augustbrandt170@gmail.com>
2024-07-09 15:21:06 +0200

    Format number values correctly Can now correctly read floats from the file after normalizing the formatting of float in csv file

--- src/reader/reader.go
+++ src/reader/reader.go
@@ -1,10 +1,14 @@
 package reader
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/csv"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gocarina/gocsv"
 )
@@ -14,9 +18,9 @@ type Transaction struct {
 	Description string  `csv:"Tekst"`
 	Type        string  `csv:"Hovedkategori"`
 	Subtype     string  `csv:"Kategori"`
-	Comment string `csv:"Kommentar"`
-	Balance float32 `csv:"Saldo"`
-	Date string `csv:"Dato"`
+	Comment     string  `csv:"Kommentar"`
+	Balance     float32 `csv:"Saldo"`
+	Date        string  `csv:"Dato"`
 }
 
 func check(e error) {
@@ -25,10 +29,52 @@ func check(e error) {
 	}
 }
 
-func ReadTransactions(filePath string) []Transaction {
-	// TODO: Find a way for the reader to correctly parse floats
-	// with formats like: x.xxx,xx
+func normalizeCsvFile(path string) error {
+	// Change floating point numbers in csv file to be
+	// formatted like: xxxx.xx instead of x.xxx,xx
 
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	var buf bytes.Buffer
+
+	scanner := bufio.NewScanner(file)
+
+	// Added csv header to buffer
+	scanner.Scan()
+	buf.Write(scanner.Bytes())
+	buf.Write([]byte("\n"))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		splitLine := strings.Split(line, ";")
+		// Normalize amount
+		s := strings.Replace(splitLine[2], ".", "", -1)
+		splitLine[2] = strings.Replace(s, ",", ".", -1)
+
+		// Normalize balance
+		s = strings.Replace(splitLine[3], ".", "", -1)
+		splitLine[3] = strings.Replace(s, ",", ".", -1)
+
+		// Write normalized line to buffer
+		newLine := strings.Join(splitLine, ";")
+		buf.Write([]byte(newLine))
+		buf.Write([]byte("\n"))
+	}
+
+	// Create normalized file in temp directory
+	err = os.WriteFile(filepath.Join(os.TempDir(), "temp.csv"), buf.Bytes(), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ReadTransactions(filePath string) []Transaction {
 	// Set up reader
 	gocsv.SetCSVReader(func(in io.Reader) gocsv.CSVReader {
 		r := csv.NewReader(in)
--- src/reader/reader_test.go
+++ src/reader/reader_test.go
@@ -2,6 +2,7 @@ package reader
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -22,9 +23,9 @@ func TestReadTransaction(t *testing.T) {
 	csvString := `Dato;Tekst;Beløb;Saldo;Hovedkategori;Kategori;Kommentar
 10.06.2024;Test Transaction 1;-150,00;600,00;Mad og indkøb;Dagligvarer;""
 10.06.2024;Test Transaction 2;5,00;658,00;Indtægter;Anden indtægt;""
-06.06.2024;Test Transaction 3;-1008,50;700,00;Transport;Taxa og offentlig transport;""
-04.06.2024;Test Transaction 4;-30,00;1234,00;Fritid;Café, restaurant og bar;""`
-	
+06.06.2024;Test Transaction 3;-108,50;700,00;Transport;Taxa og offentlig transport;""
+04.06.2024;Test Transaction 4;-30,00;234,00;Fritid;Café, restaurant og bar;""`
+
 	err := createCsvFile(csvString)
 	if err != nil {
 		t.Fatal(err)
@@ -37,5 +38,48 @@ func TestReadTransaction(t *testing.T) {
 	// Assert
 	if len(transactions) != 4 {
 		t.Errorf("Incorrect number of transactions\nExpected: 4\nGot: %d", len(transactions))
+	}
+	if transactions[0].Amount != -150.00 {
+		t.Errorf("Incorrect number for amount\nExpected; -150.00\nGot: %f", transactions[0].Amount)
+	}
+	if transactions[1].Balance != 658.00 {
+		t.Errorf("Incorrect number for balance\nExpected; 658.00\nGot: %f", transactions[1].Balance)
+	}
+}
+
+func TestNormalizeCsvFile(t *testing.T) {
+	err := normalizeCsvFile("../../data/data.csv")
+	if err != nil {
+		t.Fatalf("Error in normalizeCsvFile: %v", err)
+	}
+}
+
+func TestReadTransactionWithNormalization(t *testing.T) {
+	// Setup
+	csvString := `Dato;Tekst;Beløb;Saldo;Hovedkategori;Kategori;Kommentar
+10.06.2024;Test Transaction 1;-150,00;600,00;Mad og indkøb;Dagligvarer;""
+10.06.2024;Test Transaction 2;5,00;658,00;Indtægter;Anden indtægt;""
+06.06.2024;Test Transaction 3;-1.008,50;700,00;Transport;Taxa og offentlig transport;""
+04.06.2024;Test Transaction 4;-30,00;1.234,00;Fritid;Café, restaurant og bar;""`
+
+	err := createCsvFile(csvString)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanUp()
+
+	// Run function that gets tested
+	normalizeCsvFile("testfile.csv")
+	transactions := ReadTransactions(filepath.Join(os.TempDir(), "temp.csv"))
+
+	// Assert
+	if len(transactions) != 4 {
+		t.Errorf("Incorrect number of transactions\nExpected: 4\nGot: %d", len(transactions))
+	}
+	if transactions[2].Amount != -1008.50 {
+		t.Errorf("Incorrect number for amount\nExpected; -1008.50\nGot: %f", transactions[0].Amount)
+	}
+	if transactions[3].Balance != 1234.00 {
+		t.Errorf("Incorrect number for balance\nExpected; 1234.00\nGot: %f", transactions[1].Balance)
 	}
 }
